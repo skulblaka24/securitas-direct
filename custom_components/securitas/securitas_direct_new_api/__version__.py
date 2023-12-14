52e5fc9a5d443292531b6d077e0df277b40ce630
skulblaka24 <5969718+skulblaka24@users.noreply.github.com>
2023-12-14 14:19:33 +0100

    Update __version__.py

--- custom_components/securitas/securitas_direct_new_api/__version__.py
+++ custom_components/securitas/securitas_direct_new_api/__version__.py
@@ -1,9 +1,9 @@
 """Version."""
 
-__title__ = "securitas-direct-new-apisecuritas-direct-new-api"
+__title__ = "securitas-direct-new-apisecuritas-direct"
 __description__ = "This is the new API for securitas direct customers."
-__url__ = "https://github.com/guerrerotook/securitas-direct-new-api"
-__version__ = "0.0.2"
+__url__ = "https://github.com/skulblaka24/securitas-direct"
+__version__ = "1.0"
 __license__ = "Apache License 2"
 __author__ = ""
 __author_email__ = ""
